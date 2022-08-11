1df613cd2591ad215e62fec233614c5eef9e9174
Maksim Alekseev <angry.school.boy@mail.ru>
2022-08-11 23:18:50 +0300

    some changes

--- wp-content/uploads/pum/pum-site-scripts.js
+++ wp-content/uploads/pum/pum-site-scripts.js
@@ -1,6 +1,6 @@
 /**
  * Do not touch this file! This file created by the Popup Maker plugin using PHP
- * Last modified time: Aug 10 2022, 07:17:59
+ * Last modified time: Aug 11 2022, 07:17:43
  */
 
 
